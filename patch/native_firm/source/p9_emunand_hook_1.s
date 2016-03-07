2fac92836586b3e7cc2ec274de5ed7bdd592fab2
173210 <root.3.173210@live.com>
2016-03-07 18:39:15 +0900

    patch: native_firm: emunand: Remove redundant switch for KTR

--- patch/native_firm/source/p9_emunand_hook_1.s
+++ patch/native_firm/source/p9_emunand_hook_1.s
@@ -1,4 +1,5 @@
 @ Copyright (C) 2015 The PASTA Team
+@ Copyright (C) 2016 173210 <root.3.173210@live.com>
 @ Originally written by Roxas75
 @
 @ This program is free software; you can redistribute it and/or
@@ -15,15 +16,11 @@
 @ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 @ p9 emunand hook
-.section .patch.p9.nand.read, "a"
+.section .patch.p9.nand.write, "a"
 .thumb
 .align 2
 
 	blx	nandHook
-#ifdef PLATFORM_KTR
-	.word	0x08078751
-#else
-	.word	0x08078751
-#endif
+	.word	0x08078971
 
 .pool
